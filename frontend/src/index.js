a61cd8a10db246b77bfe48c5f95123b7bd328c13
riyaspam <riyaghadashi27@gmail.com>
2025-12-08 00:18:12 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/index.css";
+import "./styles/global.css"; // only if needed
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
